b74f65c9f53d3f6126fc8c96b49dcf1c7ec9c8fe
Mae Large <mlarge@vmware.com>
2022-09-01 15:04:54 -0700

    ChromeHeadlessCI, worked locally at least

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -36,14 +37,12 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome'],
     singleRun: true,
-    customLaunchers:{
-      HeadlessChrome:{
+    customLaunchers: {
+      ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox'
-        ]
+        flags: ['--no-sandbox']
       }
     },
     restartOnFileChange: true
